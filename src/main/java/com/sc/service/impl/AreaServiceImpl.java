7574ffcc630d4651537ae67a89c0daea0c23f2b9
fangxm <2272558745@qq.com>
2020-06-18 13:25:06 +0800

    fangjin

--- src/main/java/com/sc/service/impl/AreaServiceImpl.java
+++ src/main/java/com/sc/service/impl/AreaServiceImpl.java
@@ -3,11 +3,12 @@ package com.sc.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.sc.dao.AreaDao;
 import com.sc.entity.Area;
 import com.sc.service.AreaService;
-
+@Service
 public class AreaServiceImpl implements AreaService {
 	@Autowired
 	private AreaDao areaDao;
---
+++ src/test/java/com/sc/service/AreaServiceTest.java
@@ -0,0 +1,21 @@
+package com.sc.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sc.BaseTest;
+import com.sc.entity.Area;
+
+public class AreaServiceTest extends BaseTest{
+	@Autowired
+	private AreaService areaService;
+	@Test
+	public void testGetAreaList() {
+		List<Area> areaList = areaService.getAreaList();
+		assertEquals("东苑",areaList.get(0).getAreaName());
+	}
+}
